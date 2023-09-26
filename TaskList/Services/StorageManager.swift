e8a241342d6319f0a3983892fd0d55d12dd81419
CemenVy <semen.vydrin0108@gmail.com>
2023-09-26 18:33:54 +0400

    Added StorageManager

---
+++ TaskList/Services/StorageManager.swift
@@ -0,0 +1,16 @@
+//
+//  StorageManager.swift
+//  TaskList
+//
+//  Created by Семен Выдрин on 26.09.2023.
+//
+
+import Foundation
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    
+}
